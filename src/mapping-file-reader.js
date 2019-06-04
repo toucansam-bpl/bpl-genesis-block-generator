495a83866e9915f3c9931033b6dd957bc6e1537f
toucansam <toucansam@blockpool.io>
2019-06-04 13:14:06 -0500

    Fixed issue with mapping file reader reading last empty line

--- src/mapping-file-reader.js
+++ src/mapping-file-reader.js
@@ -3,6 +3,8 @@ const { readFileSync } = require('fs')
 module.exports = filePath =>
   readFileSync(filePath).toString().split('\n').reduce((all, line) => {
     const parts = line.split('|')
-    all[parts[1].trim()] = parts[0].trim()
+    if (parts.length === 2) {
+      all[parts[1].trim()] = parts[0].trim()
+    }
     return all
   }, {})
